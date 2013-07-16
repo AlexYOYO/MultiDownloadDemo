ca992665aa809af7c003220bd419a67ba7a68d07
AlexYOYO <alexYOYO@126.com>
2013-07-16 12:23:00 +0800

    Initial Commit

---
+++ MultiDownloadDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  MultiDownloadDemo
+//
+//  Created by Alex Q on 7/16/13.
+//  Copyright (c) 2013 Alex Qu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
